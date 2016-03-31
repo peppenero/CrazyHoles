bed5090fdbae4ba5e1d221e090eb3051a137e8f7
Giuseppe <peppenero@virgilio.it>
2016-03-31 16:23:18 +0200

    Menù terminato ideologicamente.

---
+++ CrazyHoles/src/gui/GameFrame.java
@@ -0,0 +1,94 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import Objects.GameManager;
+import Objects.WorldImpl;
+
+
+public class GameFrame extends JFrame
+{
+	private static JPanel contentPanel;
+	
+	private static MenuPanel mainMenu;
+	private static GamePanel gamePanel;
+	private static LevelEditorPanel levelEditorPanel;
+	private static SettingsPanel settingsPanel;
+	private static CreditsPanel creditsPanel;
+	
+	private GameManager gameManager;
+	final static GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
+	
+	public GameFrame(){
+		initFrame();
+	}
+	
+	public void initFrame(){
+		this.setUndecorated(true);
+		this.setSize(1280, 800);
+		this.setResizable(true);
+		this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+	}
+	
+	
+	
+	public static void main(String[] args)
+	{
+		final GameFrame frame = new GameFrame();
+		contentPanel = new JPanel(new BorderLayout());
+        frame.add(contentPanel);
+        WorldImpl world = new WorldImpl();
+        
+        gamePanel = new GamePanel(world);
+        levelEditorPanel = new LevelEditorPanel();
+        creditsPanel = new CreditsPanel();
+        settingsPanel = new SettingsPanel();
+        
+        MenuPanel menuPanel = new MenuPanel(frame);
+        switchTo(menuPanel);
+
+		device.setFullScreenWindow(frame);
+		frame.setVisible(true);
+	}
+	
+	public JPanel getGamePanel(){
+		return gamePanel;
+	}
+	
+	public JPanel getLevelEditorPanel(){
+		return levelEditorPanel;
+	}
+	
+	public JPanel getCreditsPanel() {
+		return creditsPanel;
+	}
+	
+	public JPanel getSettingsPanel(){
+		return settingsPanel;
+	}
+	
+	
+	public static void switchTo(final JPanel panel)
+    {
+        SwingUtilities.invokeLater(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    contentPanel.removeAll();
+                    contentPanel.add(panel, BorderLayout.CENTER);
+                    contentPanel.updateUI();
+                    panel.requestFocus();
+                }
+            });
+    }
+
+	
+}
---
+++ CrazyHoles/src/gui/MenuPanel.java
@@ -0,0 +1,120 @@
+package gui;
+
+
+import java.awt.Graphics;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+
+public class MenuPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	GameFrame frame;
+	
+	Image background;
+
+	Icon newGame = new ImageIcon("images/newgame.png");
+	Icon newGameSelected = new ImageIcon("images/newgame_selected.png");
+	MenuButton newGameButton = new MenuButton(newGame,newGameSelected);
+	Icon levelEditor = new ImageIcon("images/leveleditor.png");
+	Icon levelEditorSelected = new ImageIcon("images/leveleditor_selected.png");
+	MenuButton levelEditorButton = new MenuButton(levelEditor,levelEditorSelected);
+	Icon settings = new ImageIcon("images/settings.png");
+	Icon settingsSelected = new ImageIcon("images/settings_selected.png");
+	MenuButton settingsButton = new MenuButton(settings,settingsSelected);
+	Icon credits = new ImageIcon("images/credits.png");
+	Icon creditsSelected = new ImageIcon("images/credits_selected.png");
+	MenuButton creditsButton = new MenuButton(credits,creditsSelected);
+	Icon exit = new ImageIcon("images/exit.png");
+	Icon exitSelected = new ImageIcon("images/exit_selected.png");
+	MenuButton exitButton = new MenuButton(exit,exitSelected);
+
+	public MenuPanel(GameFrame frameSup){
+		frame=frameSup;
+		
+		setLayout(null);
+
+		background = Toolkit.getDefaultToolkit().createImage("images/Background.jpg");
+
+
+		newGameButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				GameFrame.switchTo(frame.getGamePanel());
+			}
+		});
+		
+		levelEditorButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				GameFrame.switchTo(frame.getLevelEditorPanel());
+			}
+		});
+
+		settingsButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				GameFrame.switchTo(frame.getSettingsPanel());
+			}
+		});
+		
+		creditsButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				GameFrame.switchTo(frame.getCreditsPanel());
+			}
+		});
+
+		exitButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				frame.setVisible(false);
+				frame.dispose();
+			}
+		});
+
+
+
+
+		add(newGameButton);
+		add(levelEditorButton);
+		add(settingsButton);
+		add(creditsButton);
+		add(exitButton);
+
+
+
+		newGameButton.setBounds(100,290,newGame.getIconWidth(),newGame.getIconHeight());
+		levelEditorButton.setBounds(100,370,newGame.getIconWidth(),newGame.getIconHeight());
+		settingsButton.setBounds(100,450,newGame.getIconWidth(),newGame.getIconHeight());
+		creditsButton.setBounds(100,530,newGame.getIconWidth(),newGame.getIconHeight());
+		exitButton.setBounds(100,610,newGame.getIconWidth(),newGame.getIconHeight());
+	}
+
+
+	public void paintComponent(Graphics g){
+
+		g.drawImage(background, 0, 0, this);
+
+	}
+}
