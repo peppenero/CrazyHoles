e94b861ac38cc2e3690849f238eff070ac978108
Giuseppe <peppenero@virgilio.it>
2016-05-24 18:26:53 +0200

    Ora da me si vede lo scoreboard

--- CrazyHoles/src/gui/ScoreBoardMenu.java
+++ CrazyHoles/src/gui/ScoreBoardMenu.java
@@ -35,28 +35,28 @@ public class ScoreBoardMenu extends JDialog
 	public ScoreBoardMenu() throws IOException, FontFormatException
 	{
 
-		   this.setModal(true);
-			layout = new BoxLayout(getContentPane(),BoxLayout.Y_AXIS);
-			this.setUndecorated(true);
-			this.setBackground(new Color(0,0,0,0));
-		   this.setSize(800,300);
-		   this.setLocation(200, 300);
-		   this.setLayout(layout);
-		   String filename = "images/ARCADE_N.TTF";
-			font = Font.createFont(Font.TRUETYPE_FONT, new File(filename));
-			font=font.deriveFont(Font.TRUETYPE_FONT,30);
-			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-			ge.registerFont(font);
-			back = new MenuButton(escIcon, escIcon);
-			back.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent arg0) {
-					// TODO Auto-generated method stub
-					dispose();
-				}
-			});
-			loadLabel();
+		this.setModal(true);
+		layout = new BoxLayout(getContentPane(),BoxLayout.Y_AXIS);
+		this.setUndecorated(true);
+		this.setBackground(new Color(255,255,255,255));
+		this.setSize(800,300);
+		this.setLocation(200, 300);
+		this.setLayout(layout);
+		String filename = "images/ARCADE_N.TTF";
+		font = Font.createFont(Font.TRUETYPE_FONT, new File(filename));
+		font=font.deriveFont(Font.TRUETYPE_FONT,30);
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		ge.registerFont(font);
+		back = new MenuButton(escIcon, escIcon);
+		back.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				dispose();
+			}
+		});
+		loadLabel();
 
 
 	}
